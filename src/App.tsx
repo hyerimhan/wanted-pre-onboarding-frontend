0c9c188b6f9ea702cb29969444d4c6c0c2f572bf
team7 <hnhyerim@gmail.com>
2023-06-13 17:51:18 +0900

    Feat: 라우터 설정 및 글로벌 스타일 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,45 @@
+import { Outlet } from 'react-router-dom'
+import { createGlobalStyle } from 'styled-components'
+import { Reset } from 'styled-reset'
+
+const GlobalStyle = createGlobalStyle`
+  * {
+    box-sizing: border-box;
+  }
+
+  #root {
+    width: 100%;
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  ul,li {
+    list-style: none;
+  }
+
+  button, input {
+    background-color: transparent;
+    border: none;
+    cursor: pointer;
+    outline: none;
+  }
+
+  input {
+    width: 100%;
+    height: 100%;
+  }
+`
+
 function App() {
-  return <div></div>
+  return (
+    <>
+      <Reset />
+      <GlobalStyle />
+      <Outlet />
+    </>
+  )
 }
 
 export default App
